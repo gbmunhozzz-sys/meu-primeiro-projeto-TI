bfded578cb1ea10c38449834b9f43b22289e49cd
GABRIEL MUNHOZ VITORINO DE ASSIS <gabriel.assis25@senaisp.edu.br>
2026-08-28 14:34:06 -0300

    feat: add my first java project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,25 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+        public class Main {
+            public static void main(String[] args) {
+                // Declara uma variável do tipo int e atribui um valor
+                int numeroInt = 42;
+
+                // Ocorre o casting implícito (promoção automática de int para double)
+                double numeroDouble = numeroInt;
+
+                // Exibe o valor da variável double no console
+                System.out.println("Valor em double: " + numeroDouble);
+            }
+        }
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            IO.println("i = " + i);
+        }
+    }
+}
