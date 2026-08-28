788e3e5552976647f2f02f381d5a42d10611c195
43836875802 <gbmunhozzz@gmail.com>
2026-08-28 16:27:43 -0300

    atividade 2 nv 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,21 +3,30 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-                // 1. Declaração de duas variáveis inteiras com valores diferentes
-                int numero1 = 15;
-                int numero2 = 8;
+                // 1. Declaração das três variáveis booleanas
+                boolean cond1 = true;
+                boolean cond2 = false;
+                boolean cond3 = true;
 
-                // Exibe os valores escolhidos
-                System.out.println("Número 1: " + numero1);
-                System.out.println("Número 2: " + numero2);
-                System.out.println("-------------------------");
+                System.out.println("cond1: " + cond1);
+                System.out.println("cond2: " + cond2);
+                System.out.println("cond3: " + cond3);
+                System.out.println("-----------------------------------");
 
-                // 2. Comparações e exibição dos resultados
-                System.out.println("numero1 > numero2  (Maior que): " + (numero1 > numero2));
-                System.out.println("numero1 < numero2  (Menor que): " + (numero1 < numero2));
-                System.out.println("numero1 >= numero2 (Maior ou igual): " + (numero1 >= numero2));
-                System.out.println("numero1 <= numero2 (Menor ou igual): " + (numero1 <= numero2));
-                System.out.println("numero1 == numero2 (Igual a): " + (numero1 == numero2));
-                System.out.println("numero1 != numero2 (Diferente de): " + (numero1 != numero2));
+                // 2. Operador E (AND) -> && (Verdadeiro se TODAS forem verdadeiras)
+                boolean resultadoAnd = cond1 && cond3;
+                System.out.println("cond1 E cond3 (cond1 && cond3): " + resultadoAnd);
+
+                // 3. Operador OU (OR) -> || (Verdadeiro se PELO MENOS UMA for verdadeira)
+                boolean resultadoOr = cond1 || cond2;
+                System.out.println("cond1 OU cond2 (cond1 || cond2): " + resultadoOr);
+
+                // 4. Operador NÃO (NOT) -> ! (Inverte o valor booleano)
+                boolean resultadoNao = !cond2;
+                System.out.println("NÃO cond2 (!cond2): " + resultadoNao);
+
+                // 5. Exemplo combinado com as três variáveis
+                boolean combinada = (cond1 || cond2) && cond3;
+                System.out.println("(cond1 OU cond2) E cond3: " + combinada);
     }
 }
