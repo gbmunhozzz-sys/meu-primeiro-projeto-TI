d6ab2cbf6c114e6980c927e63be5cae2b6af83eb
43836875802 <gbmunhozzz@gmail.com>
2026-09-01 14:24:32 -0300

    feat: atividade 3 nv 4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,16 +3,16 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-                // 1. Declara a distância percorrida (em km) e o combustível gasto (em litros)
-                double distanciaKm = 350.0;
-                double litrosGastos = 28.5;
+                // 1. Declara o valor por hora e o total de horas trabalhadas no mês
+                double valorHora = 35.50;
+                double horasTrabalhadas = 160.0;
 
-                // 2. Calcula o consumo médio (km por litro)
-                double consumoMedio = distanciaKm / litrosGastos;
+                // 2. Calcula o salário bruto (Valor por Hora * Horas Trabalhadas)
+                double salarioBruto = valorHora * horasTrabalhadas;
 
                 // 3. Exibe os resultados no console
-                System.out.println("Distância percorrida: " + distanciaKm + " km");
-                System.out.println("Total de combustível gasto: " + litrosGastos + " litros");
-                System.out.printf("Consumo médio: %.2f km/l\n", consumoMedio);
+                System.out.println("Valor por hora: R$ " + valorHora);
+                System.out.println("Horas trabalhadas no mês: " + horasTrabalhadas + "h");
+                System.out.printf("Salário bruto: R$ %.2f\n", salarioBruto);
     }
 }
