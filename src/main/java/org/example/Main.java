b1a1e5add0a898d66335429e410fb5d4766b1d45
43836875802 <gbmunhozzz@gmail.com>
2026-09-01 14:07:07 -0300

    feat: atividade 4 nv 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,14 +3,16 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-                // 1. Declara a medida do lado do quadrado
-                double lado = 5.0;
+                // 1. Declara a base e a altura do retângulo
+                double base = 8.0;
+                double altura = 4.0;
 
-                // 2. Calcule a área usando a fórmula (A = lado * lado)
-                double area = lado * lado;
+                // 2. Calcula o perímetro usando a fórmula: 2 * (base + altura)
+                double perimetro = 2 * (base + altura);
 
-                // 3. Exibe o resultado no console
-                System.out.println("Lado do quadrado: " + lado);
-                System.out.println("Área do quadrado: " + area);
+                // 3. Exibe os valores no console
+                System.out.println("Base: " + base);
+                System.out.println("Altura: " + altura);
+                System.out.println("Perímetro do retângulo: " + perimetro);
     }
 }
